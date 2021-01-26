16efafa583c57eec3fe3a237b6eec3db4ef11515
Vitaliy <60542954+vit-401@users.noreply.github.com>
2021-01-26 22:12:54 +0200

    chenge some the string

--- telegram.php
+++ telegram.php
@@ -31,7 +31,8 @@ foreach($arr as $key => $value) {
 $sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
 
 if ($sendToTelegram) {
-  header('Location: index.html');
+  // header('Location: index.html');
+  echo "Отправлено";
 } else {
   echo "Ошибка";
 }
